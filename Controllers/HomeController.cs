27798ce506ff6351e83aa183eebab175595dbf83
Lex Myers <lex.myers@gmail.com>
2017-10-24 14:16:01 -0700

    Poke Wars!

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace pokeinfo.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("gen")]
+        public IActionResult Gen(string id)
+        {
+            return RedirectToAction("Poke", new {id=id});
+        }
+
+        [HttpGet]
+        [Route("poke/{id}")]
+        public IActionResult Poke(string id)
+        {
+            var PokeInfo = new Dictionary<string, object>();
+            var SpecInfo = new Dictionary<string, object>();
+            int pokeid = Convert.ToInt32(id);
+            System.Console.WriteLine("Executing request");
+            WebRequest.GetPokemonDataAsync(pokeid, ApiResponse =>
+                {
+                    PokeInfo = ApiResponse;
+                }
+            ).Wait();
+            ViewBag.name = PokeInfo["name"];
+            ViewBag.height = PokeInfo["height"];
+            ViewBag.weight = PokeInfo["mass"];
+            string[] SpecArr = Convert.ToString(PokeInfo["species"]).Split('/');
+            string SpecId = SpecArr[5];
+            WebRequest.GetSpeciesDataAsync(SpecId, ApiResponse =>
+                {
+                    SpecInfo = ApiResponse;
+                }
+            ).Wait();
+            ViewBag.type = SpecInfo["name"];
+            return View("~/Views/Home/Poke.cshtml");
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+
+    public static class SessionExtensions
+    {
+        // We can call ".SetObjectAsJson" just like our other session set methods, by passing a key and a value
+        public static void SetObjectAsJson(this ISession session, string key, object value)
+        {
+            // This helper function simply serializes theobject to JSON and stores it as a string in session
+            session.SetString(key, JsonConvert.SerializeObject(value));
+        }
+
+        // generic type T is a stand-in indicating that we need to specify the type on retrieval
+        public static T GetObjectFromJson<T>(this ISession session, string key)
+        {
+            string value = session.GetString(key);
+            // Upon retrieval the object is deserialized based on the type we specified
+            return value == null ? default(T) : JsonConvert.DeserializeObject<T>(value);
+        }
+    }
+    public class WebRequest
+    {
+        // The second parameter is a function that returns a Dictionary of string keys to object values.
+        // If an API returned an array as its top level collection the parameter type would be "Action>"
+        public static async Task GetPokemonDataAsync(int PokeId, Action<Dictionary<string, object>> Callback)
+        {
+            // Create a temporary HttpClient connection.
+            using (var Client = new HttpClient())
+            {
+                try
+                {
+                    Client.BaseAddress = new Uri($"http://swapi.co/api/people/{PokeId}");
+                    HttpResponseMessage Response = await Client.GetAsync(""); // Make the actual API call.
+                    Response.EnsureSuccessStatusCode(); // Throw error if not successful.
+                    string StringResponse = await Response.Content.ReadAsStringAsync(); // Read in the response as a string.
+
+                    // Then parse the result into JSON and convert to a dictionary that we can use.
+                    // DeserializeObject will only parse the top level object, depending on the API we may need to dig deeper and continue deserializing
+                    Dictionary<string, object> JsonResponse = JsonConvert.DeserializeObject<Dictionary<string, object>>(StringResponse);
+
+                    // Finally, execute our callback, passing it the response we got.
+                    Callback(JsonResponse);
+                }
+                catch (HttpRequestException e)
+                {
+                    // If something went wrong, display the error.
+                    Console.WriteLine($"Request exception: {e.Message}");
+                }
+            }
+        }
+
+        public static async Task GetSpeciesDataAsync(string SpecId, Action<Dictionary<string, object>> Callback)
+        {
+            // Create a temporary HttpClient connection.
+            using (var Client = new HttpClient())
+            {
+                try
+                {
+                    Client.BaseAddress = new Uri($"http://swapi.co/api/species/{SpecId}");
+                    HttpResponseMessage Response = await Client.GetAsync(""); // Make the actual API call.
+                    Response.EnsureSuccessStatusCode(); // Throw error if not successful.
+                    string StringResponse = await Response.Content.ReadAsStringAsync(); // Read in the response as a string.
+
+                    // Then parse the result into JSON and convert to a dictionary that we can use.
+                    // DeserializeObject will only parse the top level object, depending on the API we may need to dig deeper and continue deserializing
+                    Dictionary<string, object> JsonResponse = JsonConvert.DeserializeObject<Dictionary<string, object>>(StringResponse);
+
+                    // Finally, execute our callback, passing it the response we got.
+                    Callback(JsonResponse);
+                }
+                catch (HttpRequestException e)
+                {
+                    // If something went wrong, display the error.
+                    Console.WriteLine($"Request exception: {e.Message}");
+                }
+            }
+        }
+    }
+}
